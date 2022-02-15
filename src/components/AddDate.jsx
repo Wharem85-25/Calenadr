c6727938d5dfcab37ec516e08c6ead652f92c620
Wharem85-25 <wharem85@hotmail.com>
2022-02-15 17:27:01 -0600

    Agrege el componente para poder agregar una cita

--- src/components/AddDate.jsx
+++ src/components/AddDate.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import '@styles/AddDate.scss';
 
 const AddDate = () => {
 	return (
-		<div>
-			
+		<div className="createDate">
+			<div className="form-container">
+				<h1 className="main-title">Create your appointment</h1>
+				<form action="/" className="form">
+					<div>
+						<label for="title" className="label">Title</label>
+						<input type="text" id="title" placeholder="Homework" className="input input-title" />
+						<label for="dateInit" className="label">Start Time</label>
+						<input type="date" id="dateInit" placeholder="2022-02-22" className="input input-date" />
+						<label for="dateEnd" className="label">Time of termination</label>
+						<input type="date" id="dateEnd" placeholder="2022-02-24" className="input input-date" />
+						<label for="description" className="label">Description</label>
+						<input type="text" id="description" placeholder="Very important" className="input input-description" />
+					</div>
+					<input type="submit" value="Create" className="primary-button create-button" />
+				</form>
+			</div>
 		</div>
 	);
 };
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Title from '@components/Title.jsx';
 import Calendar from '@components/Calendar.jsx';
+import AddDate from '@components/AddDate.jsx';
 
 const App = () => {
 	return (
 		<div className="container">
 			<Title />
 			<Calendar />
+			<AddDate />
 		</div>
 	);
 };
